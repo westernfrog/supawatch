e57a2fad39351e86513f0689b2706b0835043bea
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-11-10 22:48:51 +0530

    Updated

--- app/movie/page.js
+++ app/movie/page.js
@@ -4,13 +4,14 @@ import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { Fragment, useRef, useState, useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { PlayIcon } from "@heroicons/react/24/outline";
+import { EllipsisHorizontalIcon, PlayIcon } from "@heroicons/react/24/outline";
 
 export default function Movie() {
   const router = useSearchParams();
   const id = router.get("id");
   const [data, setData] = useState(null);
   const [trailer, setTrailer] = useState(null);
+  const [credits, setCredits] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -30,8 +31,8 @@ export default function Movie() {
     async function fetchTrailers() {
       try {
         const response = await fetch(`/api/getTrailer?id=${id}`);
-        const trailer = await response.json();
-        setTrailer(trailer.key);
+        const data = await response.json();
+        setTrailer(data.key);
       } catch (error) {
         console.error("Error fetching trailers:", error);
       }
@@ -39,6 +40,22 @@ export default function Movie() {
 
     fetchTrailers();
   }, [id]);
+
+  useEffect(() => {
+    async function fetchCredits() {
+      try {
+        const response = await fetch(`/api/getCredits?id=${id}`);
+        const data = await response.json();
+        setCredits(data.data);
+      } catch (error) {
+        console.error("Error fetching trailers:", error);
+      }
+    }
+
+    fetchCredits();
+  }, [id]);
+
+  console.log(credits);
 
   const [open, setOpen] = useState(false);
   const cancelButtonRef = useRef(null);
@@ -51,27 +68,28 @@ export default function Movie() {
             <div className="h-[30em]">
               <Image
                 className="w-full h-full object-cover object-center"
-                src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
+                src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
                 width={5000}
                 height={5000}
                 alt="Latest"
+                priority={true}
               />
-              <div className="absolute top-0 inset-0 bg-black/50 backdrop-opacity-100 bg-gradient-to-b from-black/40 from-10% via-black/70 via-40% to-[#010101] to-90%"></div>
+              <div className="absolute top-0 inset-0 bg-black/30 backdrop-opacity-100 bg-gradient-to-b from-black/40 from-10% via-black/70 via-40% to-[#010101] to-90%"></div>
             </div>
             <div className="absolute lg:top-20 top-14">
-              <div className="grid lg:grid-cols-12 items-start gap-12 text-gray-300 lg:px-16 p-6">
-                <div className="relative lg:col-span-3 col-span-6 rounded-xl ring-1 ring-white/20 shadow-xl">
+              <div className="flex flex-col lg:grid grid-cols-12 w-screen items-start gap-12 text-gray-300 lg:px-16 p-6">
+                <div className="relative lg:col-span-3 col-span-4 rounded-xl ring-1 ring-white/20 shadow-xl">
                   <Image
                     className="object-cover object-center rounded-xl"
-                    src={`https://image.tmdb.org/t/p/original/${data.poster_path}`}
+                    src={`https://image.tmdb.org/t/p/original${data.poster_path}`}
                     width={5000}
                     height={5000}
                     alt="Latest"
                     priority={true}
                   />
-                  <div className="absolute top-0 inset-0 bg-black/10 bg-gradient-to-b lg:bg-gradient-radial from-black/20 from-10% via-black/50 via-50% to-black/60 to-90% rounded-xl"></div>
+                  <div className="absolute top-0 inset-0 bg-black/20 rounded-xl"></div>
                 </div>
-                <div className="col-span-6 text-white w-full space-y-2">
+                <div className="col-span-7 text-white w-full space-y-2">
                   <div className="flex flex-wrap items-center gap-x-8 font-semibold text-gray-300 lg:text-base text-sm pb-2">
                     <p className="text-green-500">
                       {Math.floor(data.vote_average * 10)}% Match
@@ -84,7 +102,7 @@ export default function Movie() {
                   <p className="text-gray-300 lg:text-base text-base">
                     {data.overview}
                   </p>
-                  <div className="flex flex-wrap items-center gap-2 font-medium text-gray-300 lg:text-base text-sm pt-3 pb-4">
+                  <div className="flex flex-wrap items-center gap-2 font-medium text-gray-300 lg:text-base text-sm pt-3 pb-5">
                     {data.genres.map((genre, index) => (
                       <p
                         key={index}
@@ -94,13 +112,39 @@ export default function Movie() {
                       </p>
                     ))}
                   </div>
-                  <button
-                    onClick={() => setOpen(true)}
-                    className="bg-white lg:px-5 px-3 py-3 rounded-full flex items-center gap-2 tracking-tight text-gray-900 font-semibold"
-                  >
-                    <PlayIcon className="w-6 h-6 stroke-0 fill-gray-900" />
-                    Watch Trailer
-                  </button>
+                  <div className="">
+                    <button
+                      onClick={() => setOpen(true)}
+                      className="bg-white lg:px-5 px-3 py-3 rounded-full flex items-center gap-2 tracking-tight text-gray-900 font-semibold"
+                    >
+                      <PlayIcon className="w-6 h-6 stroke-0 fill-gray-900" />
+                      Watch Trailer
+                    </button>
+                  </div>
+                  <div className="grid grid-cols-12 flex-row flex-wrap items-center gap-6 py-6">
+                    {credits &&
+                      credits.cast.slice(0, 6).map((item, index) => (
+                        <div
+                          key={index}
+                          className="cursor-pointer lg:col-span-2 col-span-6"
+                        >
+                          <div className="relative text-gray-300">
+                            <Image
+                              className="w-full h-40 shadow shadow-white/30 object-cover object-center rounded-xl"
+                              src={`https://image.tmdb.org/t/p/original${item.profile_path}`}
+                              width={5000}
+                              height={5000}
+                              alt="Latest"
+                              priority={true}
+                            />
+                            <div className="absolute top-0 inset-0 bg-black/40 rounded-xl"></div>
+                            <h1 className="lg:absolute relative left-0 right-0 text-sm text-center py-2">
+                              {item.name}
+                            </h1>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
                 </div>
               </div>
             </div>
