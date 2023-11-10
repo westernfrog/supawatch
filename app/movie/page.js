22f2766f9f666cd59852fb5f5ee452d2070ef8fd
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-11-10 21:05:37 +0530

    Updated

--- app/movie/page.js
+++ app/movie/page.js
@@ -2,13 +2,15 @@
 
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
-import { InformationCircleIcon, PlayIcon } from "@heroicons/react/24/outline";
+import { Fragment, useRef, useState, useEffect } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { PlayIcon } from "@heroicons/react/24/outline";
 
 export default function Movie() {
   const router = useSearchParams();
   const id = router.get("id");
   const [data, setData] = useState(null);
+  const [trailer, setTrailer] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -24,63 +26,127 @@ export default function Movie() {
     fetchData();
   }, [id]);
 
+  useEffect(() => {
+    async function fetchTrailers() {
+      try {
+        const response = await fetch(`/api/getTrailer?id=${id}`);
+        const trailer = await response.json();
+        setTrailer(trailer.key);
+      } catch (error) {
+        console.error("Error fetching trailers:", error);
+      }
+    }
+
+    fetchTrailers();
+  }, [id]);
+
+  const [open, setOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
+
   return (
     <>
       {data ? (
-        <section className="relative grid h-[30em]">
-          <div className="h-[30em]">
-            <Image
-              className="w-full h-full object-cover object-center"
-              src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
-              width={5000}
-              height={5000}
-              alt="Latest"
-            />
-            <div className="absolute top-0 inset-0 bg-black/50 bg-gradient-radial from-black/40 from-10% via-black/70 via-50% to-black/90 to-90%"></div>
-          </div>
-          <div className="absolute lg:top-20 top-14">
-            <div className="grid lg:grid-cols-12 items-start gap-12 text-gray-300 lg:px-16 p-6">
-              <div className="relative lg:col-span-3 col-span-6 rounded-xl ring-1 ring-white/20 shadow-xl">
-                <Image
-                  className="object-cover object-center rounded-xl"
-                  src={`https://image.tmdb.org/t/p/original/${data.poster_path}`}
-                  width={5000}
-                  height={5000}
-                  alt="Latest"
-                />
-                <div className="absolute top-0 inset-0 bg-black/20 bg-gradient-to-b lg:bg-gradient-radial from-black/20 from-10% via-black/50 via-50% to-black/60 to-90% rounded-xl"></div>
-              </div>
-              <div className="col-span-6 text-white w-full space-y-2 py-6">
-                <div className="flex flex-wrap items-center gap-x-8 font-semibold text-gray-300 lg:text-base text-sm pb-2">
-                  <p className="text-green-500">
-                    {Math.floor(data.vote_average * 10)}% Match
-                  </p>
-                  <p>{data.release_date.slice(0, 4)}</p>
+        <>
+          <section className="relative grid h-[30em]">
+            <div className="h-[30em]">
+              <Image
+                className="w-full h-full object-cover object-center"
+                src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
+                width={5000}
+                height={5000}
+                alt="Latest"
+              />
+              <div className="absolute top-0 inset-0 bg-black/50 bg-gradient-radial from-black/40 from-10% via-black/70 via-50% to-black/90 to-90%"></div>
+            </div>
+            <div className="absolute lg:top-20 top-14">
+              <div className="grid lg:grid-cols-12 items-start gap-12 text-gray-300 lg:px-16 p-6">
+                <div className="relative lg:col-span-3 col-span-6 rounded-xl ring-1 ring-white/20 shadow-xl">
+                  <Image
+                    className="object-cover object-center rounded-xl"
+                    src={`https://image.tmdb.org/t/p/original/${data.poster_path}`}
+                    width={5000}
+                    height={5000}
+                    alt="Latest"
+                    priority={true}
+                  />
+                  <div className="absolute top-0 inset-0 bg-black/20 bg-gradient-to-b lg:bg-gradient-radial from-black/20 from-10% via-black/50 via-50% to-black/60 to-90% rounded-xl"></div>
                 </div>
-                <h1 className="text-4xl font-semibold tracking-tight">
-                  {data.title}
-                </h1>
-                <p className="text-gray-300 lg:text-lg text-base">
-                  {data.overview}
-                </p>
-                <div className="flex flex-wrap items-center gap-x-2 font-medium text-gray-300 lg:text-base text-sm pt-3 pb-6">
-                  {data.genres.map((genre) => (
-                    <p
-                      key={genre}
-                      className="bg-white/20 px-3 py-1 rounded-full text-sm"
-                    >
-                      {genre.name}
+                <div className="col-span-6 text-white w-full space-y-2 py-6">
+                  <div className="flex flex-wrap items-center gap-x-8 font-semibold text-gray-300 lg:text-base text-sm pb-2">
+                    <p className="text-green-500">
+                      {Math.floor(data.vote_average * 10)}% Match
                     </p>
-                  ))}
+                    <p>{data.release_date.slice(0, 4)}</p>
+                  </div>
+                  <h1 className="text-4xl font-semibold tracking-tight">
+                    {data.title}
+                  </h1>
+                  <p className="text-gray-300 lg:text-lg text-base">
+                    {data.overview}
+                  </p>
+                  <div className="flex flex-wrap items-center gap-x-2 font-medium text-gray-300 lg:text-base text-sm pt-3 pb-6">
+                    {data.genres.map((genre, index) => (
+                      <p
+                        key={index}
+                        className="bg-white/20 px-3 py-1 rounded-full text-sm"
+                      >
+                        {genre.name}
+                      </p>
+                    ))}
+                  </div>
+                  <button
+                    onClick={() => setOpen(true)}
+                    className="bg-white lg:px-5 px-3 py-3 rounded-full flex items-center gap-2 tracking-tight text-gray-900 font-semibold"
+                  >
+                    <PlayIcon className="w-6 h-6 stroke-0 fill-gray-900" />
+                    Watch Trailer
+                  </button>
                 </div>
-                <button className="bg-white lg:px-5 px-3 py-3 rounded-full flex items-center gap-2 tracking-tight text-gray-900 font-semibold">
-                  <PlayIcon className="w-6 h-6 stroke-0 fill-gray-900" />
-                  Watch Trailer
-                </button>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+          <Transition.Root show={open} as={Fragment}>
+            <Dialog
+              as="div"
+              className="relative z-50"
+              initialFocus={cancelButtonRef}
+              onClose={setOpen}
+            >
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" />
+              </Transition.Child>
+              <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  >
+                    <Dialog.Panel className="relative transform overflow-hidden lg:rounded-2xl rounded-lg bg-white/20 shadow-xl transition-all my-8 w-full max-w-6xl lg:h-[40em] h-64">
+                      <iframe
+                        className="w-full h-full"
+                        src={`https://www.youtube.com/embed/${trailer}`}
+                        allowFullScreen
+                      ></iframe>
+                    </Dialog.Panel>
+                  </Transition.Child>
+                </div>
+              </div>
+            </Dialog>
+          </Transition.Root>
+        </>
       ) : (
         <section className="relative h-screen grid items-center">
           <Image
