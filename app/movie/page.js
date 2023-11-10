7378f7d72dbaa3cacf6d38f9a0358117ec36476d
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-11-10 20:54:56 +0530

    Updated

--- app/movie/page.js
+++ app/movie/page.js
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
+import { InformationCircleIcon, PlayIcon } from "@heroicons/react/24/outline";
 
 export default function Movie() {
   const router = useSearchParams();
@@ -39,7 +40,7 @@ export default function Movie() {
           </div>
           <div className="absolute lg:top-20 top-14">
             <div className="grid lg:grid-cols-12 items-start gap-12 text-gray-300 lg:px-16 p-6">
-              <div className="relative lg:col-span-3 col-span-6 rounded-xl ring-1 ring-white/10 shadow-xl">
+              <div className="relative lg:col-span-3 col-span-6 rounded-xl ring-1 ring-white/20 shadow-xl">
                 <Image
                   className="object-cover object-center rounded-xl"
                   src={`https://image.tmdb.org/t/p/original/${data.poster_path}`}
@@ -49,13 +50,33 @@ export default function Movie() {
                 />
                 <div className="absolute top-0 inset-0 bg-black/20 bg-gradient-to-b lg:bg-gradient-radial from-black/20 from-10% via-black/50 via-50% to-black/60 to-90% rounded-xl"></div>
               </div>
-              <div className="col-span-6 text-white w-full space-y-2">
+              <div className="col-span-6 text-white w-full space-y-2 py-6">
+                <div className="flex flex-wrap items-center gap-x-8 font-semibold text-gray-300 lg:text-base text-sm pb-2">
+                  <p className="text-green-500">
+                    {Math.floor(data.vote_average * 10)}% Match
+                  </p>
+                  <p>{data.release_date.slice(0, 4)}</p>
+                </div>
                 <h1 className="text-4xl font-semibold tracking-tight">
                   {data.title}
                 </h1>
                 <p className="text-gray-300 lg:text-lg text-base">
                   {data.overview}
                 </p>
+                <div className="flex flex-wrap items-center gap-x-2 font-medium text-gray-300 lg:text-base text-sm pt-3 pb-6">
+                  {data.genres.map((genre) => (
+                    <p
+                      key={genre}
+                      className="bg-white/20 px-3 py-1 rounded-full text-sm"
+                    >
+                      {genre.name}
+                    </p>
+                  ))}
+                </div>
+                <button className="bg-white lg:px-5 px-3 py-3 rounded-full flex items-center gap-2 tracking-tight text-gray-900 font-semibold">
+                  <PlayIcon className="w-6 h-6 stroke-0 fill-gray-900" />
+                  Watch Trailer
+                </button>
               </div>
             </div>
           </div>
