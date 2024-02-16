0eeda5bdbd3c3ee9546e0739720820cdabe90e8f
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2024-02-16 23:13:27 +0530

    Updated

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -43,64 +43,69 @@ export default function Movie() {
               <div className="absolute top-0 inset-0 lg:bg-black/30 bg-gradient-to-b from-black/60 from-20% via-black/50 via-40% to-[#010101] to-98%"></div>
             </div>
             <div className="absolute inset-10 lg:inset-52 flex items-center justify-center">
-              <div className="flex-wrap flex flex-col items-center lg:gap-6 gap-4">
+              <div className="flex-wrap flex flex-col items-center gap-2">
                 <h1 className="lg:text-8xl text-5xl font-bold text-dm text-center opacity-80">
                   {data.title}
                 </h1>
-                <button
-                  type="button"
-                  onClick={() => setPlay(true)}
-                  className="flex-shrink-0 flex items-center gap-3"
-                >
-                  <div className="backdrop-blur-xl bg-white/10 lg:p-4 p-2 rounded-full active:scale-95 transition duration-300 ease-in-out">
-                    <PlayIcon className="lg:w-10 lg:h-10 w-7 h-7 ps-1" />
-                  </div>
-                  <h1 className="font-medium lg:text-lg tracking-tight">
-                    Play Movie
-                  </h1>
-                </button>
-                <Transition.Root show={play} as={Fragment}>
-                  <Dialog
-                    as="div"
-                    className="relative z-50"
-                    initialFocus={cancelButtonRef}
-                    onClose={setPlay}
-                  >
-                    <Transition.Child
-                      as={Fragment}
-                      enter="ease-out duration-300"
-                      enterFrom="opacity-0"
-                      enterTo="opacity-100"
-                      leave="ease-in duration-200"
-                      leaveFrom="opacity-100"
-                      leaveTo="opacity-0"
-                    >
-                      <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" />
-                    </Transition.Child>
-                    <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
-                      <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
-                        <Transition.Child
-                          as={Fragment}
-                          enter="ease-out duration-300"
-                          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                          enterTo="opacity-100 translate-y-0 sm:scale-100"
-                          leave="ease-in duration-200"
-                          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                          leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                          <Dialog.Panel className="relative transform overflow-hidden lg:rounded-2xl rounded-lg bg-white/20 shadow-xl transition-all my-8 w-full max-w-6xl lg:h-[40em] h-64">
-                            <iframe
-                              className="w-full h-full"
-                              src={`https://multiembed.mov/?video_id=${id}&tmdb=1`}
-                              allowFullScreen
-                            ></iframe>
-                          </Dialog.Panel>
-                        </Transition.Child>
-                      </div>
-                    </div>
-                  </Dialog>
-                </Transition.Root>
+                <p className="lg:text-2xl text-lg font-medium text-center opacity-80 tracking-tight">
+                  {data.tagline}
+                </p>
               </div>
+            </div>
+            <div className="absolute bottom-10 inset-x-0 flex items-center justify-center">
+              <button
+                type="button"
+                onClick={() => setPlay(true)}
+                className="flex-shrink-0 flex items-center gap-3"
+              >
+                <div className="backdrop-blur-xl bg-white/10 lg:p-4 p-2 rounded-full active:scale-95 transition duration-300 ease-in-out">
+                  <PlayIcon className="lg:w-10 lg:h-10 w-7 h-7 ps-1" />
+                </div>
+                <h1 className="font-medium lg:text-lg tracking-tight">
+                  Play Movie
+                </h1>
+              </button>
+              <Transition.Root show={play} as={Fragment}>
+                <Dialog
+                  as="div"
+                  className="relative z-50"
+                  initialFocus={cancelButtonRef}
+                  onClose={setPlay}
+                >
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" />
+                  </Transition.Child>
+                  <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                      >
+                        <Dialog.Panel className="relative transform overflow-hidden lg:rounded-2xl rounded-lg bg-white/20 shadow-xl transition-all my-8 w-full max-w-6xl lg:h-[40em] h-64">
+                          <iframe
+                            className="w-full h-full"
+                            src={`https://multiembed.mov/?video_id=${id}&tmdb=1`}
+                            allowFullScreen
+                          ></iframe>
+                        </Dialog.Panel>
+                      </Transition.Child>
+                    </div>
+                  </div>
+                </Dialog>
+              </Transition.Root>
             </div>
           </section>
           {data || data?.success ? (
