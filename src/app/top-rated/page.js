49369707d6fae38df9afff85218b11d4a8d149ec
westernfrog <haaamansingh007@gmail.com>
2025-12-26 16:58:55 +0530

    Updated

--- src/app/top-rated/page.js
+++ src/app/top-rated/page.js
@@ -10,7 +10,7 @@ export default function TopRated() {
       apiEndpoint="/api/getMovieList?list=top_rated"
       linkPrefix="/movie"
       emptyMessage="No more movies"
-      heroImage="https://images.unsplash.com/photo-1511875762315-c773eb98eec0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      heroImage="https://images.unsplash.com/photo-1569035560433-6113350a78a6?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     />
   );
 }
